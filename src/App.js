930c4841298b5f9514a83dee30a081192b2b63f9
Ozan Bayiz <ozanbayiz@gmail.com>
2024-10-23 10:42:55 -0700

    updated publications

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Publications from './pages/Publications';
 import Projects from './pages/Projects';
 import CV from './pages/CV';
-function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(false);
 
-  React.useEffect(() => {
-    const checkIsMobile = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    checkIsMobile();
-    window.addEventListener('resize', checkIsMobile);
-
-    return () => window.removeEventListener('resize', checkIsMobile);
-  }, []);
-
-  return isMobile;
-}
 
 function App() {
-  const isMobile = useIsMobile();
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home isMobile={isMobile} />} />
-        <Route path="/about" element={<About isMobile={isMobile} />} />
-        <Route path="/projects" element={<Projects isMobile={isMobile} />} />
-        <Route path="/publications" element={<Publications isMobile={isMobile} />} />
-        <Route path="/cv" element={<CV isMobile={isMobile} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/publications" element={<Publications />} />
+        <Route path="/cv" element={<CV />} />
       </Routes>
     </Router>
   );
--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { isMobile } from 'react-device-detect';
+import Blink from 'react-blink-text';
 
 function Publications() {
     const fontSize = isMobile ? '12px' : '16px';
@@ -15,6 +16,7 @@ function Publications() {
             </nav>
             <div className="header-line"></div>
             <div className="container">
+                <Blink text="You've Just Been Pranked" />
                 <img src="/nyan_cat.gif"/>
             </div>
         </div>
